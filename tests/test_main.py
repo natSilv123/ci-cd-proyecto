dece09a33438bc2626fc1c7b330ff717bda0da47
natSilv123 <na.tis00@hotmail.com>
2024-12-14 13:09:39 -0500

    Corrige flujo de trabajo de CI/CD 2

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,5 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from app.main import suma
-
+from app.main import suma  
+# Importamos la función a probar
 # Función de prueba con pytest
 def test_suma():
     assert suma(2, 3) == 5  # Esperamos que 2 + 3 sea 5
